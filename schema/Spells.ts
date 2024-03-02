72e6376845e08ec7a8c58b2645cbb6258d9ced40
Edurne <herenvardo@gmail.com>
2024-03-02 10:58:27 +0100

    feat: basic validation for Spell assets

---
+++ schema/Spells.ts
@@ -0,0 +1,27 @@
+interface RawSpellBase {
+    range?: string;
+    duration?: string;
+    castingTime?: string;
+    area?: string;
+    target?: any;
+    trigger?: any;
+}
+
+interface RawSpellRank extends RawSpellBase {
+    rank: number;
+    damage?: any;
+    dot?: any;
+    heal?: any;
+}
+
+interface RawSpellAsset extends RawSpellBase {
+    key: string;
+    ranks: RawSpellRank[];
+    sch: string;
+    components: string[];
+    save?: any;
+    flags?: string[];
+    descriptors?: string[];
+}
+
+export type { RawSpellAsset };
