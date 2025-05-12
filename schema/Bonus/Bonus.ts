b98a4a1a44ee5365c4414bd555b51f0ae6c99a32
Edurne Pascual <herenvardo@gmail.com>
2025-05-12 12:32:00 +0200

    feat(#39): add RawVitals and update Races

--- schema/Bonus/Bonus.ts
+++ schema/Bonus/Bonus.ts
@@ -1,5 +1,11 @@
-import { DamageType, Save, Skill, Stat } from '@wowfinder/ts-enums';
-import type { RawSpellPower } from './SpellPower';
+import {
+    type DamageType,
+    type Save,
+    type Skill,
+    type Stat,
+} from '@wowfinder/ts-enums';
+import { type RawSpellPower } from './SpellPower';
+import { type RawVitals } from './Vitals';
 
 type RawResistances = {
     [key in DamageType]: number;
@@ -24,6 +30,7 @@ interface RawBonus {
     skills?: Partial<RawSkills>;
     resistances?: Partial<RawResistances>;
     saves?: Partial<RawSaves>;
+    vitals?: Partial<RawVitals>;
 }
 
 export type { RawBonus, RawResistances, RawSaves, RawSkills, RawStats };
